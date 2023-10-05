0c51009aa4a15f186a2771b931c38c79dd0146bf
David Luu <dluu.audeo@gmail.com>
2023-10-05 00:35:40 -0700

    corrected pagejs with updated mapping and terminology for dropdown in sections

--- dluu-ga-project/app/components/Section.js
+++ dluu-ga-project/app/components/Section.js
@@ -1,13 +1,21 @@
 import DropdownOptions from "./DropdownOptions"
 
-export default function Section() {
+export default function Section(props) {
     return (
         <div>
-            <h1>section title</h1>
-            <div className="grid grid-cols-2">
-                <DropdownOptions />
-                <p>question 1</p>
+        <h1>{props.section.sectionTitle}</h1>
+        {props.section.questions.map((questions, i) => {
+          return (
+            <div className="grid grid-cols-2" key={i}>
+              <DropdownOptions identifier={props.section.sectionTitle + "" + i} />
+              <div className="grid">
+                <text>{questions}</text>
+                {/* <textarea>Write message here</textarea> */}
+              </div>
             </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+    );
+    
 }
