456e8e75b7306f8745ebfb694b3c5fb76e8f35d9
David Luu <dluu.audeo@gmail.com>
2023-10-05 00:25:42 -0700

    mapped sections - node updates disabled testing

--- dluu-ga-project/app/page.js
+++ dluu-ga-project/app/page.js
@@ -2,14 +2,16 @@ import Image from 'next/image'
 import Header from './components/Header'
 import AvgScore from './components/AvgScore'
 import Section from './components/Section'
+import data from './data/questions.json'
 
 export default function Home() {
   return (
     <main>
       <Header />
       <AvgScore />
-      <Section />
-      <Section />
+      {data.Sections.map((section, i) => (
+          <Section key={i} section={section} />
+        ))}
       <AvgScore />
     </main>
   )
