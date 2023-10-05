7087b4bae015456322a58fa34bfb9f172c4a7d0b
David Luu <dluu.audeo@gmail.com>
2023-10-04 23:50:53 -0700

    dropdown basic format

--- dluu-ga-project/app/components/DropdownOptions.js
+++ dluu-ga-project/app/components/DropdownOptions.js
@@ -1,5 +1,9 @@
 export default function DropdownOptions() {
     return (
-        <h1>Dropdown Options</h1>
+        <select>
+            <option value="NA">No answer</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+        </select>
     )
 }
